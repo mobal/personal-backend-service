7a793a97c71a34d729d9838d8d4f59db7dc07ccc
Balázs Molnár <bmolnar0510@gmail.com>
2022-06-06 23:46:21 +0200

    Refactor

--- app/api/v1/posts/models.py
+++ app/api/v1/posts/models.py
@@ -9,7 +9,7 @@ class Post(CamelModel):
     author: str
     title: str
     content: str
-    _created_at: datetime
+    created_at: datetime
     deleted_at: Optional[datetime] = None
     published_at: Optional[datetime] = None
     updated_at: Optional[datetime] = None
