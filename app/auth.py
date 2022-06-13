aaa3eff58122a9fe619c1770b4efec6e28cdc4eb
Balázs Molnár <bmolnar0510@gmail.com>
2022-06-13 08:40:48 +0200

    Added logging

--- app/auth.py
+++ app/auth.py
@@ -25,16 +25,20 @@ class JWTAuth(HTTPBearer):
         credentials = await super(JWTAuth, self).__call__(request)
         if credentials:
             if credentials.scheme != 'Bearer':
+                self.logger.error(f'Invalid authentication scheme credentials={credentials}')
                 raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid authentication scheme')
             if not self.validate_token(credentials.credentials):
+                self.logger.error(f'Invalid authentication token credentials={credentials}')
                 raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid authentication token')
             return credentials.credentials
         else:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid authorization code')
+            self.logger.error(f'Credentials missing during authentications')
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def validate_token(self, token: str) -> bool:
         try:
             decoded_token = jwt.decode(token, self.config.jwt_secret, algorithms='HS256')
-        except InvalidSignatureError:
+        except InvalidSignatureError as error:
+            self.logger.error(f'Failed to validate JWT token signature error={error}')
             return False
         return True if pendulum.from_timestamp(decoded_token['exp']) > pendulum.now() else False
