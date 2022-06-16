30ec2df9750c658641062ad6ebfac7d585d50938
Balázs Molnár <bmolnar0510@gmail.com>
2022-06-16 19:58:29 +0200

    Refactor

--- app/services/post.py
+++ app/services/post.py
@@ -15,16 +15,17 @@ from app.models.post import Post
 from app.config import Configuration
 
 
-def _slugify(title: str) -> str:
+def create_slug(title: str) -> str:
     return f'{slugify(title)}-{hashlib.sha1(title.encode(encodings.utf_8.getregentry().name)).hexdigest()[:10]}'
 
 
 class PostService:
-    logger = logging.getLogger()
-    config = Configuration()
-    session = boto3.Session()
-    dynamodb = session.resource('dynamodb')
-    table = dynamodb.Table(f'{config.app_stage}-posts')
+    def __init__(self) -> None:
+        self.logger = logging.getLogger()
+        config = Configuration()
+        session = boto3.Session()
+        dynamodb = session.resource('dynamodb')
+        self.table = dynamodb.Table(f'{config.app_stage}-posts')
 
     def _delete_post_by_uuid(self, uuid: str) -> None:
         post = self._get_post_by_uuid(uuid)
@@ -54,7 +55,7 @@ class PostService:
     def create_post(self, data: dict) -> Post:
         post = Post(id=str(uuid.uuid4()), author=data['author'], title=data['title'], content=data['content'],
                     created_at=pendulum.now().to_iso8601_string(), published_at=data['published_at'],
-                    slug=_slugify(data['title']))
+                    slug=create_slug(data['title']))
         self.table.put_item(Item=post.dict())
         self.logger.info(f'Post successfully created post={post}')
         return post
