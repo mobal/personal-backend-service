0cbbc2b0bfe67889e6a3932dfd5d56fad5a1db77
Balázs Molnár <bmolnar0510@gmail.com>
2022-07-03 23:57:19 +0200

    Refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
+import uuid
+
 import boto3
+import pendulum
 import pytest
 from moto import mock_dynamodb
 
+from app.auth import JWTToken
 from app.config import Configuration
 from app.services.cache import CacheService
 
@@ -20,3 +24,13 @@ def config() -> Configuration:
 def dynamodb_client():
     with mock_dynamodb():
         yield boto3.resource('dynamodb')
+
+
+@pytest.fixture
+def jwt_token() -> JWTToken:
+    now = pendulum.now()
+    return JWTToken(exp=now.add(hours=1).int_timestamp, iat=now.int_timestamp, iss='https://netcode.hu',
+                    jti=str(uuid.uuid4()), sub={'id': str(uuid.uuid4()), 'email': 'info@netcode.hu',
+                                                'display_name': 'root', 'roles': ['root'],
+                                                'created_at': now.to_iso8601_string(), 'deleted_at': None,
+                                                'updated_at': None})
