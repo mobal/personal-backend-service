cfb8179106665c7f901792e0d2a3fd5762110a55
Balázs Molnár <bmolnar0510@gmail.com>
2022-07-05 10:05:55 +0200

    Refactor

--- utils/coverage.py
+++ utils/coverage.py
@@ -8,10 +8,27 @@ from xml.etree import ElementTree
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-parser = ArgumentParser(description='Fix source paths in generated coverage XML file')
-parser.add_argument('-c', '--coverage-file', required=True, type=str, help='path of the coverage file')
-parser.add_argument('-f', '--find', required=True, type=str, help='path to find for replace')
-parser.add_argument('-r', '--replace', required=True, type=str, help='path to replace')
+parser = ArgumentParser(
+    description='Fix source paths in generated coverage XML file')
+parser.add_argument(
+    '-c',
+    '--coverage-file',
+    required=True,
+    type=str,
+    help='path of the coverage file')
+parser.add_argument(
+    '-f',
+    '--find',
+    required=True,
+    type=str,
+    help='path to find for replace')
+parser.add_argument(
+    '-r',
+    '--replace',
+    required=True,
+    type=str,
+    help='path to replace')
+
 
 def _load_coverage(path: str) -> ElementTree:
     with open(path, 'r') as f:
@@ -20,10 +37,10 @@ def _load_coverage(path: str) -> ElementTree:
 
 def _replace_source(
         et: ElementTree,
-        old_source: str,
-        new_source: str) -> ElementTree:
+        find: str,
+        replace: str) -> ElementTree:
     for el in et.iter('source'):
-        el.text = el.text.replace(old_source, new_source)
+        el.text = el.text.replace(find, replace)
     return et
 
 
@@ -32,17 +49,17 @@ def _save(et: ElementTree, path: str):
         et.write(f, encoding='utf-8')
 
 
-def main(cov_file: str, from_source: str, to_source: str):
-    if exists(cov_file) is False:
-        logger.error(f'File {cov_file} is not exists')
+def main(coverage_file: str, path_to_find: str, path_to_replace: str):
+    if exists(coverage_file) is False:
+        logger.error(f'File coverage_file={coverage_file} is not exists')
         sys.exit(-2)
-    logger.info(f'Loading cov_file={cov_file}')
-    et = _load_coverage(cov_file)
+    logger.info(f'Load coverage_file={coverage_file}')
+    et = _load_coverage(coverage_file)
     logger.info(
-        f'Replaces source values from old_source{from_source} to new_source={to_source}')
-    et = _replace_source(et, old_source=from_source, new_source=to_source)
-    logger.info(f'Save cov_file={cov_file}')
-    _save(et, cov_file)
+        f'Find and replace source path_to_find={path_to_find} to path_to_replace={path_to_replace}')
+    et = _replace_source(et, find=path_to_find, replace=path_to_replace)
+    logger.info(f'Save cov_file={coverage_file}')
+    _save(et, coverage_file)
 
 
 if __name__ == '__main__':
