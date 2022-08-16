52c1f8cbd2d70ca74be25d15037a76173c002460
Balázs Molnár <bmolnar0510@gmail.com>
2022-08-16 10:00:38 +0200

    Refactor

--- tests/unit/service/test_cache_service.py
+++ tests/unit/service/test_cache_service.py
@@ -1,11 +1,11 @@
 import uuid
 import pendulum
 import pytest
+from httpx import Response
 from starlette import status
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip(reason='For testing')
 class TestCacheService:
     key_value = {
         'key': str(uuid.uuid4()),
@@ -14,12 +14,12 @@ class TestCacheService:
     }
 
     async def test_successfully_get_key_value(
-        self, cache_service, settings, httpx_mock
+        self, cache_service, settings, respx_mock
     ):
-        httpx_mock.add_response(
-            url=f'{settings.cache_service_base_url}/api/cache/{self.key_value["key"]}',
-            status_code=status.HTTP_200_OK,
-            json=self.key_value,
+        respx_mock.get(
+            f'{settings.cache_service_base_url}/api/cache/{self.key_value["key"]}'
+        ).mock(
+            return_value=Response(status_code=status.HTTP_200_OK, json=self.key_value)
         )
         result = await cache_service.get(self.key_value['key'])
         assert bool(result) is True
@@ -28,17 +28,20 @@ class TestCacheService:
         assert self.key_value['value'] == result.value
 
     async def test_successfully_get_key_value_with_invalid_id(
-        self, cache_service, settings, httpx_mock
+        self, cache_service, settings, respx_mock
     ):
         message = f'The requested value was not found for key={self.key_value["key"]}'
-        httpx_mock.add_response(
-            url=f'{settings.cache_service_base_url}/api/cache/{self.key_value["key"]}',
-            status_code=status.HTTP_404_NOT_FOUND,
-            json={
-                'status': status.HTTP_404_NOT_FOUND,
-                'id': self.key_value['key'],
-                'message': message,
-            },
+        respx_mock.get(
+            f'{settings.cache_service_base_url}/api/cache/{self.key_value["key"]}'
+        ).mock(
+            Response(
+                status_code=status.HTTP_404_NOT_FOUND,
+                json={
+                    'status': status.HTTP_404_NOT_FOUND,
+                    'id': self.key_value['key'],
+                    'message': message,
+                },
+            ),
         )
         result = await cache_service.get(self.key_value['key'])
         assert result is None
