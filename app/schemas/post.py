3d442df605ee1a0b47483f27b15e8c588620e568
Balázs Molnár <797870+mobal@users.noreply.github.com>
2022-08-27 21:02:57 +0200

    Markdown (#35)
    * Added markdown==3.4.1 package
    
    * Develop (#33)
    
    * Create codeql-analysis.yml
    
    * Added role handling
    
    * Fixed typo
    
    * Refactor
    
    * Added deploy job
    
    * Added field filtering
    
    * Convert content to html as default
    
    * Use dicts only during repository calls

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from fastapi_camelcase import CamelModel
-from pydantic import conlist, constr
+from pydantic import conlist, constr, Extra
 
 from app.models.post import Meta
 
@@ -10,10 +10,15 @@ class CreatePost(CamelModel):
     author: constr(strip_whitespace=True, min_length=3)
     title: constr(strip_whitespace=True, min_length=3)
     content: constr(strip_whitespace=True, min_length=3)
-    tags: Optional[conlist(str, min_items=1)]
-    meta: Optional[Meta]
-    published_at: Optional[str]
+    tags: Optional[conlist(str, min_items=1)] = None
+    meta: Optional[Meta] = None
+    published_at: Optional[str] = None
+
+    class Config:
+        extra = Extra.forbid
 
 
 class UpdatePost(CreatePost):
-    pass
+    author: Optional[constr(strip_whitespace=True, min_length=3)]
+    title: Optional[constr(strip_whitespace=True, min_length=3)]
+    content: Optional[constr(strip_whitespace=True, min_length=3)]
