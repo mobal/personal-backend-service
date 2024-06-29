5bfeb413208b1c4d43878d4ce1dbdb94e821e38f
Balázs Molnár <bmolnar0510@gmail.com>
2024-06-29 09:47:04 +0000

    feat: Added pytest-env plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import os
 import uuid
 from random import randint
 
@@ -14,25 +13,6 @@ from app.settings import Settings
 def pytest_configure():
     pytest.cache_service_base_url = "https://localhost"
     pytest.jwt_secret = "6fl3AkTFmG2rVveLglUW8DOmp8J4Bvi3"
-
-
-def pytest_sessionstart():
-    os.environ["DEBUG"] = "true"
-    os.environ["LOG_LEVEL"] = "DEBUG"
-    os.environ["STAGE"] = "test"
-
-    os.environ["APP_NAME"] = "personal-backend-service"
-    os.environ["APP_TIMEZONE"] = "Europe/Budapest"
-
-    os.environ["CACHE_SERVICE_BASE_URL"] = pytest.cache_service_base_url
-    os.environ["JWT_SECRET"] = pytest.jwt_secret
-
-    os.environ["AWS_REGION"] = "eu-central-1"
-    os.environ["AWS_ACCESS_KEY_ID"] = "aws_access_key_id"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "aws_secret_access_key"
-
-    os.environ["POWERTOOLS_LOGGER_LOG_EVENT"] = "true"
-    os.environ["POWERTOOLS_SERVICE_NAME"] = "personal-backend-service"
 
 
 @pytest.fixture
