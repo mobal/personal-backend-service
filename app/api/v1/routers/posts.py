e43a48f47aa0ac6420cf6a8856db5fb7cdc049f0
Balázs Molnár <bmolnar0510@gmail.com>
2024-07-06 10:18:04 +0200

    refactor: Removed unnecessary null check

--- app/api/v1/routers/posts.py
+++ app/api/v1/routers/posts.py
@@ -82,13 +82,10 @@ async def get_post_by_uuid(uuid: str) -> PostResponse:
     "",
     response_model=Page,
     status_code=status.HTTP_200_OK,
+    response_model_exclude_none=True,
 )
 async def get_posts(exclusive_start_key: str | None = None) -> Page:
-    return (
-        await post_service.get_posts(exclusive_start_key)
-        if exclusive_start_key
-        else await post_service.get_posts()
-    )
+    return await post_service.get_posts(exclusive_start_key)
 
 
 @router.put(
