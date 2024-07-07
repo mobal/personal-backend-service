da3154bf05b15a08bc3de0ea246421aab3029516
Balázs Molnár <bmolnar0510@gmail.com>
2024-07-07 22:54:08 +0200

    feat: added rate limiting middleware

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,17 +1,23 @@
 import uuid
 from contextvars import ContextVar
+from datetime import datetime, timedelta
 
 from aws_lambda_powertools import Logger
+from starlette import status
 from starlette.middleware.base import (BaseHTTPMiddleware,
                                        RequestResponseEndpoint)
 from starlette.requests import Request
-from starlette.responses import Response
+from starlette.responses import JSONResponse, Response
 from starlette.types import ASGIApp
 
+from app import Settings
+
 X_CORRELATION_ID = "X-Correlation-ID"
+
 correlation_id: ContextVar[str | None] = ContextVar(
     X_CORRELATION_ID, default=str(uuid.uuid4())
 )
+settings = Settings()
 
 
 class CorrelationIdMiddleware(BaseHTTPMiddleware):
@@ -28,3 +34,34 @@ class CorrelationIdMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         response.headers[X_CORRELATION_ID] = correlation_id.get()
         return response
+
+
+class RateLimitingMiddleware(BaseHTTPMiddleware):
+    RATE_LIMIT_DURATION = timedelta(seconds=settings.rate_limit_duration)
+
+    def __init__(self, app):
+        super().__init__(app)
+        self.requests = {}
+
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        if settings.debug is False:
+            client_ip = request.client.host
+            request_count, last_request = self.requests.get(
+                client_ip, (0, datetime.min)
+            )
+            elapsed_time = datetime.now() - last_request
+            if elapsed_time > self.RATE_LIMIT_DURATION:
+                request_count = 1
+            else:
+                if request_count >= settings.rate_limit_requests:
+                    return JSONResponse(
+                        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+                        content={
+                            "message": "Rate limit exceeded. Please try again later"
+                        },
+                    )
+                request_count += 1
+            self.requests[client_ip] = (request_count, datetime.now())
+        return await call_next(request)
--- app/settings.py
+++ app/settings.py
@@ -9,4 +9,6 @@ class Settings(BaseSettings):
     aws_secret_access_key: str
     jwt_secret: str
     cache_service_base_url: str
+    rate_limit_duration: int
+    rate_limit_requests: int
     stage: str
