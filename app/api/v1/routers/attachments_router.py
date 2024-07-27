0e4cc0095ea3514d4c6e636b69d5f00863e559e3
Balázs Molnár <bmolnar0510@gmail.com>
2024-07-27 08:47:00 +0200

    feat: added get attachment api endpoint

--- app/api/v1/routers/attachments_router.py
+++ app/api/v1/routers/attachments_router.py
@@ -1,5 +1,8 @@
+from io import BytesIO
+
 from aws_lambda_powertools import Logger
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import StreamingResponse
 
 from app.jwt_bearer import JWTBearer
 from app.services.storage_service import StorageService
@@ -9,3 +12,13 @@ logger = Logger(utc=True)
 jwt_bearer = JWTBearer()
 storage_service = StorageService()
 router = APIRouter()
+
+
+@router.get("/{bucket}/{key}", status_code=status.HTTP_200_OK)
+async def get_attachment(bucket: str, key: str) -> StreamingResponse:
+    response = StreamingResponse(
+        BytesIO(await storage_service.get_object(bucket, key)),
+        media_type="application/octet-stream",
+    )
+    response.headers["Content-Disposition"] = "attachment;"
+    return response
