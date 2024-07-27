8d99908f88fbbf95c24ef4b273f8308c5517808a
Balázs Molnár <bmolnar0510@gmail.com>
2024-07-27 08:46:46 +0200

    refactor: added return types

--- app/services/storage_service.py
+++ app/services/storage_service.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import boto3
 from aws_lambda_powertools import Logger
 
@@ -9,9 +11,9 @@ class StorageService:
         self.__logger = Logger(utc=True)
         self.__s3_resource = boto3.resource("s3", region_name=settings.aws_region)
 
-    async def create_bucket(self, bucket: str, acl: str = "private"):
+    async def create_bucket(self, bucket: str, acl: str = "private") -> dict[str, Any]:
         self.__logger.info(f"Creating {bucket=} with {acl=}")
-        self.__s3_resource.create_bucket(
+        return self.__s3_resource.create_bucket(
             ACL=acl,
             Bucket=bucket,
             CreateBucketConfiguration={
@@ -19,15 +21,15 @@ class StorageService:
             },
         )
 
-    async def delete_object(self, bucket: str, key: str):
+    async def delete_object(self, bucket: str, key: str) -> dict[str, Any]:
         self.__logger.info(f"Delete object {key=} from {bucket=}")
         return self.__s3_resource.Object(bucket_name=bucket, key=key).delete()
 
-    async def get_object(self, bucket: str, key: str):
+    async def get_object(self, bucket: str, key: str) -> dict[str, Any]:
         self.__logger.info(f"Get object {key=} from {bucket=}")
         return self.__s3_resource.Object(bucket_name=bucket, key=key).get()
 
-    async def list_objects(self, bucket: str) -> list:
+    async def list_objects(self, bucket: str) -> list[dict[str, Any]]:
         self.__logger.info(f"Listing {bucket=}")
         return self.__s3_resource.Bucket(name=bucket).objects.all()
 
@@ -38,10 +40,10 @@ class StorageService:
         body: bytes,
         acl: str = "public-read",
         metadata: dict[str, str] = {},
-    ):
+    ) -> dict[str, Any]:
         self.__logger.info(
             f"Put object with {key=} and {acl=} into {bucket=}", metadata=metadata
         )
-        self.__s3_resource.Object(bucket_name=bucket, key=key).put(
+        return self.__s3_resource.Object(bucket_name=bucket, key=key).put(
             Body=body, Metadata=metadata
         )
