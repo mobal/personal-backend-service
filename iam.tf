733dc4114b5f8583da4fd0d694bed128a43609f6
Balázs Molnár <bmolnar0510@gmail.com>
2024-08-02 17:17:57 +0200

    feat: added vpc

--- iam.tf
+++ iam.tf
@@ -64,3 +64,21 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
+
+resource "aws_security_group" "lambda_security_groups" {
+  vpc_id = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
