c2b17895ee71073a265d18d1bdc93615603bda22
Balázs Molnár <bmolnar0510@gmail.com>
2025-08-24 09:30:39 +0200

    fix: replaced deprecated regex with pattern

--- app/api/v1/routers/posts_router.py
+++ app/api/v1/routers/posts_router.py
@@ -45,10 +45,10 @@ def get_archive() -> dict[str, Any]:
 @router.get("/{year}/{month}/{day}/{slug}", status_code=status.HTTP_200_OK)
 def get_by_post_path(
     slug: str,
-    year: str = Path(regex=r"^\d{4}$", description="4 digit year"),
-    month: str = Path(regex=r"^(0[1-9]|1[0-2])$", description="2 digit month (01-12)"),
+    year: str = Path(pattern=r"^\d{4}$", description="4 digit year"),
+    month: str = Path(pattern=r"^(0[1-9]|1[0-2])$", description="2 digit month (01-12)"),
     day: str = Path(
-        regex=r"^(0[1-9]|[12]\d|3[01])$", description="2 digit day (01-31)"
+        pattern=r"^(0[1-9]|[12]\d|3[01])$", description="2 digit day (01-31)"
     ),
 ) -> PostResponse:
     year_int = int(year)
