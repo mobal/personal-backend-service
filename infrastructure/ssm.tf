201f6dd3da780f3d03deee6c24c9f75b3419a77d
Balázs Molnár <797870+mobal@users.noreply.github.com>
2026-04-07 20:54:45 +0200

    fix: ssm param name

--- infrastructure/ssm.tf
+++ infrastructure/ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "apigw_url" {
-  name      = "/${var.stage}/auth-service/api-gateway/url"
+  name      = "/${var.stage}/personal-backend-service/api-gateway/url"
   type      = "String"
   value     = aws_apigatewayv2_api.http_api.api_endpoint
   overwrite = true
